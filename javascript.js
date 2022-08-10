0e019cfba8d782f83725d09e3d70355d4cc4e145
Ann Shen Cheah <cheah.annshen@gmail.com>
2022-08-11 03:31:33 +0800

    Completed game implementation and logic, QoL to html and css
    TODO: Fix game function (see comments)

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 console.log("Hello World! Rock Paper Scissors Time!");
 
+// * Variable to keep track of the score, starts at 0
+let playerScore = 0;
+let computerScore = 0;
+
 // * getComputerChoice randomly returns a "choice" for the computer.
 function getComputerChoice() {
   const choice = ["rock", "paper", "scissors"];
@@ -21,35 +25,89 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  // * Switch statement used for the logic of the game.
+  // * Switch statement is used for the logic of the game.
+  // * Points are given to either player or computer depending on the outcome.
   switch (playerSelection) {
     case "rock":
-      if (computerSelection === "rock") return "Tie! Rock and Rock.";
-      else if (computerSelection === "paper")
+      if (computerSelection === "rock") {
+        return "Tie! Rock and Rock.";
+      } else if (computerSelection === "paper") {
+        computerScore++;
         return "You Lose! Paper beats Rock";
-      else return "You Win! Rock beats Scissors";
+      } else {
+        playerScore++;
+        return "You Win! Rock beats Scissors";
+      }
       break;
 
     case "paper":
-      if (computerSelection === "rock") return "You Win! Paper beats Rock";
-      else if (computerSelection === "paper") return "Tie! Paper and Paper";
-      else return "You lose! Scissors beat Paper";
+      if (computerSelection === "rock") {
+        playerScore++;
+        return "You Win! Paper beats Rock";
+      } else if (computerSelection === "paper") {
+        return "Tie! Paper and Paper";
+      } else {
+        computerScore++;
+        return "You Lose! Scissors beat Paper";
+      }
       break;
 
     case "scissors":
-      if (computerSelection === "rock") return "You Lose! Rock beats Scissors";
-      else if (computerSelection === "paper")
+      if (computerSelection === "rock") {
+        computerScore++;
+        return "You Lose! Rock beats Scissors";
+      } else if (computerSelection === "paper") {
+        playerScore++;
         return "You Win! Scissors beat Paper";
-      else return "Tie! Scissors and Scissors";
+      } else {
+        return "Tie! Scissors and Scissors";
+      }
       break;
   }
 }
 
-// TODO: Complete game function.
+// TODO: Fix error handling when invalid input is detected.
+// TODO: Game will restart from Round 1 if invalid answer is given mid-game.
+// * game function is called to start the game
 function game() {
-  const computerSelection = getComputerChoice();
+  for (i = 0; i < 5; i++) {
+    // * Get computer choice each time it is called
+    const computerSelection = getComputerChoice();
 
-  while (playerSelection == null || playerSelection == "") {
-    const playerSelection = prompt("Rock, Paper or Scissors?", "").toLowerCase;
+    // * Prompt user for a valid answer
+    let playerSelection = prompt(`Round ${i + 1}\nRock Paper Scissors?`, "");
+    if (playerSelection == null) {
+      alert("Cancelled");
+      break;
+    }
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "") {
+      alert("Please pick a hand.");
+    } else if (
+      playerSelection != "rock" &&
+      playerSelection != "scissors" &&
+      playerSelection != "paper"
+    ) {
+      alert("Please enter a valid input.");
+    } else {
+      // * If user gives a valid answer, call playRound function to determine winner
+      console.log(playRound(playerSelection, computerSelection));
+    }
   }
+  calculateScore();
+}
+
+// * Function to calculate the final score and determine the winner.
+function calculateScore() {
+  if (playerScore > computerScore)
+    console.log(
+      `You are the winner! Player Scored ${playerScore} to Computer Score ${computerScore}`
+    );
+  else if (playerScore < computerScore)
+    console.log(
+      `You lost! Player Score ${playerScore} to Computer Score ${computerScore}`
+    );
+  else console.log(`Tie Game! The score is tied at ${playerScore}`);
 }
