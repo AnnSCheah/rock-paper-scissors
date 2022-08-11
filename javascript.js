5101e8635e3ef7278bb622df63c8a799f830c1de
Ann Shen Cheah <cheah.annshen@gmail.com>
2022-08-12 01:50:20 +0800

    Revised comments for better readability and cleaner code

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 console.log("Hello World! Rock Paper Scissors Time!");
 
-// * Variable to keep track of the score, starts at 0
 let playerScore = 0;
 let computerScore = 0;
 
-// * getComputerChoice randomly returns a "choice" for the computer.
 function getComputerChoice() {
   const choice = ["rock", "paper", "scissors"];
 
@@ -30,9 +28,8 @@ function getComputerChoice() {
   return choice[computerChoice];
 }
 
+// * The logic of the game
 function playRound(playerSelection, computerSelection) {
-  // * Switch statement is used for the logic of the game.
-  // * Points are given to either player or computer depending on the outcome.
   switch (playerSelection) {
     case "rock":
       if (computerSelection === "rock") {
@@ -72,7 +69,7 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// * game function is called to start the game
+// * The function to start the game.
 function game() {
   // * Setting up the game
   let validAnswer = false;
@@ -81,12 +78,9 @@ function game() {
   computerScore = 0;
 
   for (i = 0; i < 5; i++) {
-    // * Get computer choice each time it is called
     const computerSelection = getComputerChoice();
 
-    // * Prompt user for a valid answer
-    // * If an invalid answer is given, the while loop will prompt the user
-    // * without the for loop incrementing by 1
+    // * Prompt user for a valid answer using while loop
     while (!validAnswer) {
       playerSelection = prompt(`Round ${i + 1}\nRock Paper Scissors?`, "");
 
@@ -95,9 +89,9 @@ function game() {
         return; // * Breaks out of the function and nested loops
       }
 
+      // * To make user input case insensitive
       playerSelection = playerSelection.toLowerCase();
 
-      // * Logic check for valid answer
       if (
         playerSelection == "" ||
         (playerSelection != "rock" &&
@@ -106,20 +100,20 @@ function game() {
       ) {
         alert("Please enter a valid input.");
       } else {
-        validAnswer = true; // * Changed to true to break out of while loop
+        validAnswer = true;
       }
     }
 
     // * playRound function is called to determine winner
     console.log(playRound(playerSelection, computerSelection));
 
-    validAnswer = false; // * Changed to false for the next round
-    playerSelection = ""; // * Reset playerSelection to ''
+    // * Resetting variable for next round
+    validAnswer = false;
+    playerSelection = "";
   }
   calculateScore();
 }
 
-// * Function to calculate the final score and determine the winner.
 function calculateScore() {
   if (playerScore > computerScore)
     console.log(
